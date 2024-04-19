4818793e2ab2664123f99920a5d27a45fbec1aba
TommyW427 <99832839+TommyW427@users.noreply.github.com>
2024-04-19 18:11:55 -0400

    Implemented everything in python. RANSAC did not perform as well as polygonal analysis

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import cv2, sys
 import numpy as np
+from imageHelper import pad, pad_all
 
 from ellipseDetector import ellipseDetector
 
+def getBestEllipse(ellipseHelper, Contour):
+    return ellipseHelper.fitEllipse_Polygon(Contour)
 
 def main():
     saveImages = True
     displayImages = True
+    
+    
     
     fileName = "unlabaled3.jpg"
     thresh = 210
@@ -18,29 +23,45 @@ def main():
             thresh = int(sys.argv[i+1])
     
     im_gray = cv2.imread(fileName, cv2.IMREAD_GRAYSCALE)
+    
     binary = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1] 
+    
     
     #Otsu Test Can Be Implemented to Guess Threshold Values#
     #im_bw = cv2.threshold(im_gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
     
-    if (saveImages): cv2.imwrite("imThresh.png", binary)
+    
     
     contourImage = cv2.imread(fileName)
+    ellipseImage = cv2.imread(fileName)
     
+    print(binary.shape)
+    print(contourImage.shape)
+    print(contourImage)
     
+    im_gray, binary, contourImage, ellipseImage = pad_all([im_gray, binary, contourImage, ellipseImage])
+    
+    if (saveImages): cv2.imwrite("imThresh.png", binary)
+    
+    print(binary)
+    print(binary.shape, contourImage.shape)
+    #print(contourImage)
     
     contours = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
     cv2.drawContours(contourImage, contours, -1, (0,0,0), 3)
     if (saveImages): cv2.imwrite("imContours.png", contourImage);
     
-    ellipseImage = cv2.imread(fileName)
+    print("savedContours");
+    print(len(contours));
+
     
     #RANSAC Approach O(n) Ellipse Fitting per Contour
     ellipseHelper = ellipseDetector()
     for contour in contours: 
         if len(contour)>=5:
-            bestEllipse = ellipseHelper.fitEllipse_RANSAC(contour)
-            cv2.ellipse(ellipseImage, bestEllipse, (255,0,255), 1)
+            bestEllipse = getBestEllipse(ellipseHelper, contour)
+            if not bestEllipse == None: 
+                cv2.ellipse(ellipseImage, bestEllipse, (255,0,255), 1)
     
     cv2.imwrite("imParasites.png", ellipseImage);
     
